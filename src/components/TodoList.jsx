4b2dbbc116c80075d1374f80397ab018d6edd87b
Kaai Suzuki <suzydp@me.com>
2018-06-16 16:02:13 -0700

    Make details of TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 export default class TodoList extends Component{
   render(){
+
+    //todosを後ほどreduxから読み取る
+
+    const fakeTodos = [
+      {id:1, text:"buy a car", completed:false, todoClick:()=>console.log("clicked!")},
+      {id:2, text:"play the guitar", completed:true, todoClick:()=>console.log("clicked!")},
+      {id:3, text:"play with cat", completed:false, todoClick:()=>console.log("clicked!")}
+    ];
+
     return(
-      <div>
-        Todo List
-      </div>
+      <ul>
+        {fakeTodos.map(todo =>
+          <li
+            key = {todo.id}
+            onClick = {todo.todoClick}
+            style = {{
+              textDecoration: todo.completed ? 'line-through' : 'none'
+            }}
+          >
+            {todo.text}
+          </li>
+        )}
+      </ul>
     )
   }
 }
